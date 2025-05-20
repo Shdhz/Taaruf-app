373ecf56874777faa6ec0247c23ad5e085bb3443
ShDhz <zombiephoenix43@gmail.com>
2025-05-21 00:06:48 +0700

    Feat: front-end for home page and explore page

--- lib/pages/beranda.dart
+++ lib/pages/beranda.dart
@@ -18,7 +18,6 @@ class _BerandaState extends State<Beranda> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
         title: Text('Home', style: TextStyle(color: Colors.deepPurple)),
         backgroundColor: const Color.fromARGB(255, 255, 255, 255),
         elevation: 0,
@@ -32,94 +31,170 @@ class _BerandaState extends State<Beranda> {
                   horizontal: 16,
                   vertical: 8,
                 ),
-                backgroundColor: const Color.fromARGB(255, 244, 212, 253),
+                backgroundColor: Colors.deepPurple,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(8),
                 ),
               ),
               child: const Text(
-                'Edit Profil',
-                style: TextStyle(color: Colors.deepPurple),
+                'CV Taaruf',
+                style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
               ),
             ),
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Row(
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 14),
+          child: Column(
             children: [
+              // Profile widget
               Container(
-                margin: const EdgeInsets.all(10),
-                width: 70,
-                height: 70,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(color: Colors.grey.shade300, width: 1),
-                  image: const DecorationImage(
-                    image: AssetImage('images/download.jpg'),
-                    fit: BoxFit.cover,
-                  ),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 15,
                 ),
-              ),
-              const SizedBox(width: 12),
-
-              // User info
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(
                   children: [
-                    Text('Agus Rohmat', style: AppTextStyle.h3),
-                    const SizedBox(height: 4),
-                    Text(
-                      'agus@gmail.com',
-                      style: AppTextStyle.bodyMedium.copyWith(
-                        color: Colors.grey.shade600,
+                    Container(
+                      margin: const EdgeInsets.all(10),
+                      width: 70,
+                      height: 70,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.grey.shade300,
+                          width: 1,
+                        ),
+                        image: const DecorationImage(
+                          image: AssetImage('images/download.jpg'),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                    const SizedBox(height: 4),
-                    Row(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 10,
-                            vertical: 4,
+                    const SizedBox(width: 12),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Agus Rohmat', style: AppTextStyle.h3),
+                          const SizedBox(height: 4),
+                          Text(
+                            'agus@gmail.com',
+                            style: AppTextStyle.bodyMedium.copyWith(
+                              color: Colors.grey.shade600,
+                            ),
                           ),
-                          decoration: BoxDecoration(
-                            color: const Color.fromARGB(255, 252, 243, 218),
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: Row(
+                          const SizedBox(height: 4),
+                          Row(
                             children: [
-                              const Icon(
-                                Icons.star,
-                                color: Colors.amber,
-                                size: 14,
-                              ),
-                              const SizedBox(width: 4),
-                              Text(
-                                'Gold Member',
-                                style: AppTextStyle.bodySmall.copyWith(
-                                  color: const Color.fromARGB(255, 24, 24, 24),
+                              Container(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 10,
+                                  vertical: 4,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: const Color.fromARGB(
+                                    255,
+                                    252,
+                                    243,
+                                    218,
+                                  ),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                child: Row(
+                                  children: [
+                                    const Icon(
+                                      Icons.star,
+                                      color: Colors.amber,
+                                      size: 14,
+                                    ),
+                                    const SizedBox(width: 4),
+                                    Text(
+                                      'Gold Member',
+                                      style: AppTextStyle.bodySmall.copyWith(
+                                        color: const Color.fromARGB(
+                                          255,
+                                          24,
+                                          24,
+                                          24,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
                             ],
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 16),
+
+              // Quick Button container
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 15,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: GridView.count(
+                  crossAxisCount: 3,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  crossAxisSpacing: 15,
+                  mainAxisSpacing: 20,
+                  children: [
+                    _buildMenuItem(Icons.settings, 'Pengaturan'),
+                    _buildMenuItem(Icons.help, 'Bantuan'),
+                    _buildMenuItem(Icons.report, 'Laporkan'),
+                    _buildMenuItem(Icons.person, 'Profil'),
+                    _buildMenuItem(Icons.logout, 'Keluar'),
+                    _buildMenuItem(Icons.auto_stories, 'Cara kerja'),
                   ],
                 ),
               ),
             ],
           ),
-        ],
+        ),
       ),
       bottomNavigationBar: CustomBottomNav(),
     );
   }
 }
 
-
-
-// Masih ngebug gabisa pindah ke halaman lain
+Widget _buildMenuItem(IconData icon, String label) {
+  return SizedBox(
+    child: Column(
+      children: [
+        CircleAvatar(
+          radius: 28,
+          backgroundColor: const Color.fromARGB(255, 239, 236, 245),
+          child: Icon(
+            icon,
+            color: const Color.fromARGB(255, 168, 124, 243),
+            size: 28,
+          ),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          label,
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontSize: 13),
+        ),
+      ],
+    ),
+  );
+}
--- lib/pages/find_couple.dart
+++ lib/pages/find_couple.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../widget/bottomnav/bottom_nav.dart';
 
@@ -15,10 +16,105 @@ class _FindCoupleState extends State<FindCouple> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('Explore'),
-        backgroundColor: Colors.deepPurpleAccent,
+        title: Text('Explore', style: TextStyle(color: Colors.deepPurple)),
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        elevation: 0,
+        actions: [
+          IconButton(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            onPressed: () {},
+            icon: Icon(Icons.tune, size: 28),
+          ),
+        ],
       ),
-      body: Text("Explore"),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              // container filter calon
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 15,
+                ),
+                decoration: BoxDecoration(color: Colors.white),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.deepPurple,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 10,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      child: Text(
+                        'Trending 🔥',
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          color: const Color.fromARGB(255, 255, 255, 255),
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    
+                    TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 247, 243, 253),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 10,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      child: Text(
+                        'Likes You',
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          color: const Color.fromARGB(255, 7, 7, 7),
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+
+                    TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 247, 243, 253),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 10,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      child: Text(
+                        'Visited You',
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          color: const Color.fromARGB(255, 7, 7, 7),
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              // Widget Calon taaruf
+            ],
+          ),
+        ),
+      ),
       bottomNavigationBar: CustomBottomNav(),
     );
   }
