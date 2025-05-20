c46349bc3215cc3f43e9a4348a0e1b707f2f022f
ShDhz <zombiephoenix43@gmail.com>
2025-05-20 19:30:14 +0700

    add: widget profile in home screen

--- lib/pages/beranda.dart
+++ lib/pages/beranda.dart
@@ -1,8 +1,7 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
 
+import '../theme/app_text_style.dart';
 import '../widget/bottomnav/bottom_nav.dart';
 
 class Beranda extends StatefulWidget {
@@ -14,25 +13,107 @@ class Beranda extends StatefulWidget {
 
 class _BerandaState extends State<Beranda> {
   int selectedIndex = 0;
-  Random random = Random();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('Home'),
-        backgroundColor: Colors.deepPurpleAccent,
+        title: Text('Home', style: TextStyle(color: Colors.deepPurple)),
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        elevation: 0,
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 10),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 8,
+                ),
+                backgroundColor: const Color.fromARGB(255, 244, 212, 253),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              child: const Text(
+                'Edit Profil',
+                style: TextStyle(color: Colors.deepPurple),
+              ),
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        child: const Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("kontol"),
-            SizedBox(width: 20,),
-            Text("MEMEK")
-          ],
-        ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.all(10),
+                width: 70,
+                height: 70,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.grey.shade300, width: 1),
+                  image: const DecorationImage(
+                    image: AssetImage('images/download.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 12),
+
+              // User info
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Agus Rohmat', style: AppTextStyle.h3),
+                    const SizedBox(height: 4),
+                    Text(
+                      'agus@gmail.com',
+                      style: AppTextStyle.bodyMedium.copyWith(
+                        color: Colors.grey.shade600,
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Row(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 10,
+                            vertical: 4,
+                          ),
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 252, 243, 218),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Row(
+                            children: [
+                              const Icon(
+                                Icons.star,
+                                color: Colors.amber,
+                                size: 14,
+                              ),
+                              const SizedBox(width: 4),
+                              Text(
+                                'Gold Member',
+                                style: AppTextStyle.bodySmall.copyWith(
+                                  color: const Color.fromARGB(255, 24, 24, 24),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
       bottomNavigationBar: CustomBottomNav(),
     );
